26bb43efd2e9679c8fccbab4fdab1d2db33d8519
Nerwin Hernando <nerwinc.hernando@gmail.com>
2024-09-09 04:40:11 +0800

    navbar

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
     get '/', to: 'dashboard#index', as: :root
     resources :users
     resources :products
+    get 'settings', to: 'settings#index', as: :settings
   end
+  delete 'logout', to: 'sessions#destroy', as: :logout
 end
