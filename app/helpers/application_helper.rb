d10077caa31b03c6f1f64d7fd38e67a9c3728ec3
Nerwin Hernando <nerwinc.hernando@gmail.com>
2024-09-09 05:14:05 +0800

    lint fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,9 +3,9 @@ module ApplicationHelper
     content_for :breadcrumbs do
       args.each_with_index do |arg, index|
         if index == args.size - 1
-          concat content_tag(:li, arg, class: 'breadcrumb-item active', 'aria-current' => 'page')
+          concat content_tag(:li, arg, class: "breadcrumb-item active", "aria-current" => "page")
         else
-          concat content_tag(:li, link_to(arg[:text], arg[:path]), class: 'breadcrumb-item')
+          concat content_tag(:li, link_to(arg[:text], arg[:path]), class: "breadcrumb-item")
         end
       end
     end
