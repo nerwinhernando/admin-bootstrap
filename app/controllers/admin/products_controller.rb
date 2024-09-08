cb8b798bfd0e2fc56ec38371d164a92811023dab
Nerwin Hernando <nerwinc.hernando@gmail.com>
2024-09-09 05:43:18 +0800

    render list of products

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,13 +1,48 @@
 class Admin::ProductsController < ApplicationController
   layout "admin"
-
   def index
-    # Add logic to list users
+    @products = Product.page(params[:page]).per(10)
   end
 
   def show
     @product = Product.find(params[:id])
   end
 
-  # Add other actions as needed
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to admin_product_path(@product), notice: "Product was successfully created."
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to admin_product_path(@product), notice: "Product was successfully updated."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to admin_products_path, notice: "Product was successfully deleted."
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price, :stock, :category)
+  end
 end
